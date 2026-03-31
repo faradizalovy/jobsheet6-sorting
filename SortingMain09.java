06cfde6e083931e95f1dde3f974aaf83b19a5cdd
faradizalovy <lovyfaradiza@gmail.com>
2026-03-31 09:10:53 +0700

    Praktikum 1

---
+++ SortingMain09.java
@@ -0,0 +1,28 @@
+public class SortingMain09 {
+    public static void main(String[] args) {
+        int a[] = {20, 10, 2, 7, 12};
+        int b[] = {30, 20, 2, 8, 14};
+        int c[] = {40, 10, 4, 9, 3};
+
+        Sorting09 dataurut1 = new Sorting09(a, a.length);
+        System.out.println("Data awal 1");
+        dataurut1.tampil();
+        dataurut1.bubbleSort();
+        System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
+        dataurut1.tampil();
+
+        Sorting09 dataurut2 = new Sorting09(b, b.length);
+        System.out.println("Data awal 2");
+        dataurut2.tampil();
+        dataurut2.bubbleSort();
+        System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
+        dataurut2.tampil();
+
+        Sorting09 dataurut3 = new Sorting09(c, c.length);
+        System.out.println("Data awal 3");
+        dataurut3.tampil();
+        dataurut3.bubbleSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+        dataurut3.tampil();
+    }
+}
